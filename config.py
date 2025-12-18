b90c9923f326542c77ae8df5f51da7a2fc93c444
z3r0tru5t <119681083+z3r0tru5t@users.noreply.github.com>
2025-12-18 07:57:10 +0100

    config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-N_SAMPLES = 200   # możesz dać więcej niż kolega
+N_SAMPLES = 200  
 N_EPOCHS = 3
 SEL_MODEL = "qwen"
 BATCH_SIZE = 1
 GRAD_ACCUM = 2
-MAX_LEN = 1024  # lub 1536 jeśli działa
+MAX_LEN = 1024 
